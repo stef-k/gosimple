14c0f21bc988c2a3a17cce14b9a5bc270c1e8d0e
Stef Kariotidis <stef.kariotidis@gmail.com>
2016-09-21 11:51:24 +0300

    setup database and user model

--- models/models.go
+++ models/models.go
@@ -1,1 +1,58 @@
 package models
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	//_ "github.com/go-sql-driver/mysql" // uncomment appropriately
+	//_ "github.com/lib/pq" // uncomment appropriately
+	_ "github.com/mattn/go-sqlite3"
+	"fmt"
+)
+
+// sync database (makes migrations)
+func generateTables()  {
+	name := "default"
+	force := true
+	verbose := false
+	orm.RunSyncdb(name, force, verbose)
+}
+
+// to use your app's models with the ORM, you must first
+// register them. Put all your app's models here
+func registerModels() {
+	orm.RegisterModel(new(User))
+}
+
+// check and set which database to use
+func dbInit() {
+	database := beego.AppConfig.DefaultString("database", "sqlite3")
+	dbname := beego.AppConfig.DefaultString("dbname", "database")
+	if database == "sqlite3" {
+		dbname += ".db"
+	}
+	if database == "sqlite3" {
+		orm.RegisterDataBase("default", "sqlite3", "./" + dbname)
+	} else {
+		dbuser := beego.AppConfig.String("dbuser")
+		dbpass := beego.AppConfig.String("dbpass")
+		dbaddress := beego.AppConfig.String("dbaddress")
+		dbport := beego.AppConfig.String("dbport")
+		dbMaxIdleCons, _ := beego.AppConfig.Int("dbMaxIdleCons")
+		if database == "mysql" {
+			orm.RegisterDataBase("default", "mysql",
+				fmt.Sprintf("%v:%v@tcp/%v", dbuser, dbpass, dbname),
+				dbMaxIdleCons)
+		} else if database == "postgres" {
+			orm.RegisterDataBase("default", "postgres",
+				fmt.Sprintf("postgresql://%v:%v@%v:%v", dbuser, dbpass, dbaddress, dbport),
+				dbMaxIdleCons)
+		}
+	}
+}
+
+// initialize ORM and app models
+func init() {
+	dbInit()
+	registerModels()
+	generateTables()
+}
---
+++ models/user.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type User struct {
+	Id             int
+	Username       string `orm:"unique"`
+	Password       string
+	Email          string `orm:"unique"`
+	EmailConfirmed bool
+	Created        time.Time `orm:"auto_now_add;type(datetime)"`
+	Usertype       string // use this to set different user types, such as superadmin, admin, user, etc
+	Active         bool   // use to lock down the account
+}
