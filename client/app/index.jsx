66294d03761c6a055190195121d2e18d48315b06
Stef Kariotidis <stef.kariotidis@gmail.com>
2016-09-27 10:44:16 +0300

    change address and port

--- client/app/index.jsx
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-import {render} from 'react-dom';
-import {Router, browserHistory} from 'react-router'
-
-import Routes from 'Routes/Routes';
-
-render(
-  <Router  routes={Routes}  history={browserHistory}/>,
-  document.getElementById("app"));
