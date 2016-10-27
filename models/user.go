c7a27d5dc1d0b2458f81d9843f24ce3f66ac3b8d
Stef Kariotidis <stef.kariotidis@gmail.com>
2016-10-27 19:24:58 +0300

    more comments

--- models/user.go
+++ models/user.go
@@ -10,7 +10,9 @@ type User struct {
 	EmailConfirmed   bool
 	ConfirmationCode string // used during registration confirmation
 	Created          time.Time `orm:"auto_now_add;type(datetime)"`
-	Usertype         string // use this to set different user types, such as superadmin, admin, user, etc
+                            // use this to set different user types, such as Admin, Staff, User, etc
+                            // for more complex situtations a Roles and a Roles_Permissions models could be more of help
+	Usertype         string
 	Active           bool   // use to lock down the account
 }
 
