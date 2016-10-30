0319c8934f825228225139e272713200f792522a
Stef Kariotidis <stef.kariotidis@gmail.com>
2016-10-30 21:43:05 +0200

    GET login controller, further limiter integration

--- controllers/login.go
+++ controllers/login.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stef-k/gosimple/models"
 	"time"
 	"github.com/stef-k/gosimple/limiter"
+	"fmt"
 )
 
 type LoginController struct {
@@ -24,8 +25,17 @@ type loginParameters struct {
 // @Failure 400 bad request
 // @router /login/ [get]
 func (lc *LoginController) ShowLoginTemplate()  {
-	lc.Data["json"] = "You must imlement the login form in this controller"
-	lc.ServeJSON()
+	limiterEnabled := beego.AppConfig.DefaultBool("limiter::loginLimiterEnabled", true)
+	incomingIP := lc.Ctx.Input.IP()
+	lockMinutes, _ := beego.AppConfig.Int("limiter::loginLockMinutes")
+	// if limiter is enabled and login limit reached, show login locked...
+	if limiter.LoginLimitReached(incomingIP) && limiterEnabled {
+		lc.Data["json"] = "Login limit has been reached, please wait " + fmt.Sprintf("%v", lockMinutes) + " minutes to retry."
+		lc.ServeJSON()
+	} else {
+		lc.Data["json"] = "You must imlement the login form in this controller"
+		lc.ServeJSON()
+	}
 }
 
 // @Title login
@@ -38,20 +48,33 @@ func (lc *LoginController) Login() {
 	isLoggedin := lc.GetSession(sessionName) != nil
 
 	if !isLoggedin {
+		limiterEnabled := beego.AppConfig.DefaultBool("limiter::loginLimiterEnabled", true)
+		incomingIP := lc.Ctx.Input.IP()
+		lockMinutes, _ := beego.AppConfig.Int("limiter::loginLockMinutes")
+		if limiter.LoginLimitReached(incomingIP) && limiterEnabled {
+			lc.Data["json"] = "Login limit has been reached, please wait " + fmt.Sprintf("%v", lockMinutes) + " minutes to retry."
+			lc.ServeJSON()
+		}
 		var params loginParameters
 		json.Unmarshal(lc.Ctx.Input.RequestBody, &params)
-		isAuthenticated, user := models.AuthenticateUser(params.UsernameOrEmail, params.Password)
+		isAuthenticated, user, error := models.AuthenticateUser(params.UsernameOrEmail, params.Password)
 		if isAuthenticated {
 			userInfo := make(map[string]interface{})
 			userInfo["username"] = user.Username
 			userInfo["role"] = user.Role
 			userInfo["timestamp"] = time.Now()
 			lc.SetSession(sessionName, userInfo)
+			lc.Data["json"] = "Welcome " + user.Username
 		} else {
-			//flash := beego.NewFlash()
-			//flash.Error("Your username/email and/or password are incorrect")
-			limitReached, attempts, timestamp := limiter.RecordLoginAttempt(params.UsernameOrEmail, lc.Ctx.Input.IP())
-			beego.Warning("Login Limit reached? ", limitReached, " remaining attempts: ", attempts, " time: ", timestamp)
+			// with rate limiter enabled
+			if limiterEnabled {
+				_, attempts, _ := limiter.RecordLoginAttempt(params.UsernameOrEmail, incomingIP)
+				lc.Data["json"] = "Login failed, please try again. Reason: " + error + " Remaining attempts: " + fmt.Sprintf("%v", attempts)
+			} else {
+				// with rate limiter disabled
+				lc.Data["json"] = "Login failed, please try again. Reason: " + error
+			}
+
 		}
 	}
 	lc.ServeJSON()
